0e5390c6f8985a46689214f0d011436c5fc7dcad
Maria Dayekh <mariaaldayekh@gmail.com>
2022-11-20 19:12:48 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+
+const main = document.getElementById("main");
+main.remove();
+
+const body = document.getElementsByTagName('body');
+const node = document.createElement("div");
+
+var newHeader = document.createElement("h1");
+node.appendChild (newHeader);
+
+newHeader.id = 'victory';
+newHeader.textContent= "Maria is the champion";
+
+
